250bca50c9a207e7d7ef5c9af7e0f8320a4fc28f
neildan <44823148+neildan@users.noreply.github.com>
2021-02-27 11:23:23 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from './components/app/app-routing.module';
+import { AppComponent } from './components/app/app.component';
 
 @NgModule({
+  /** Components */
   declarations: [
     AppComponent
   ],
+  /** Modules */
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+  /** Services */
   providers: [],
+  /** Main component */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
